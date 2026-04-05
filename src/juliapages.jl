7ac27c8f0c809b73cc7a27105677136f3fea2498
Jesse E <jesse.d.everington@gmail.com>
2026-04-05 17:56:01 -0400

    update

---
+++ src/juliapages.jl
@@ -0,0 +1,15 @@
+module juliapages
+
+# Julia Pages - A comprehensive mathematics learning platform built with Pluto notebooks
+# 
+# This module serves as the package definition for the JuliaPages documentation system.
+# Individual tracks and lessons are organized as Pluto notebooks in the tracks/ directory.
+
+using Pluto
+using PlutoUI
+using PlutoPages
+
+# Package exports
+export juliapages
+
+end # module
